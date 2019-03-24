7ee906685f6ab904ac2aaba69238436c15b2c2e3
RobDWaller <rdwaller1984@googlemail.com>
2019-03-24 16:50:06 +0000

    Added tests and basic outline for JWTAuthProcess class for psr-15 middleware.

--- src/JwtAuthProcess.php
+++ src/JwtAuthProcess.php
@@ -4,7 +4,25 @@ declare(strict_types=1);
 
 namespace PsrJwt;
 
-class JwtAuthProcess
+use PsrJwt\JwtAuth;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class JwtAuthProcess extends JwtAuth implements MiddlewareInterface
 {
-    
+    public function __construct(string $secret)
+    {
+        parent::__construct($secret);
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $token = $this->getToken($request);
+
+        $this->validate($token);
+
+        return $handler->handle($request);
+    }
 }
--- tests/JwtAuthProcessTest.php
+++ tests/JwtAuthProcessTest.php
@@ -3,7 +3,13 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use PsrJwt\Jwt;
 use PsrJwt\JwtAuthProcess;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Mockery as m;
 
 class JwtAuthProcessTest extends TestCase
 {
@@ -12,8 +18,37 @@ class JwtAuthProcessTest extends TestCase
      */
     public function testJwtAuthProcess()
     {
-        $process = new JwtAuthProcess();
+        $process = new JwtAuthProcess('secret');
 
         $this->assertInstanceOf(JwtAuthProcess::class, $process);
+        $this->assertInstanceOf(MiddlewareInterface::class, $process);
+    }
+
+    public function testProcess()
+    {
+        $jwt = Jwt::builder();
+        $token = $jwt->setSecret('Secret123!456$')
+            ->setIssuer('localhost')
+            ->setPayloadClaim('nbf', time() - 60)
+            ->build()
+            ->getToken();
+
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->twice()
+            ->andReturn(['jwt' => $token]);
+
+        $response = m::mock(ResponseInterface::class);
+
+        $handler = m::mock(RequestHandlerInterface::class);
+        $handler->shouldReceive('handle')
+            ->once()
+            ->andReturn($response);
+
+        $process = new JwtAuthProcess('Secret123!456$');
+
+        $result = $process->process($request, $handler);
+
+        $this->assertInstanceOf(ResponseInterface::class, $result);
     }
 }
