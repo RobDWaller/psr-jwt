29384a2252822786f5dfbe1fc88ecb9d04cfc35b
RobDWaller <rdwaller1984@googlemail.com>
2019-04-04 13:49:32 +0100

    Fixed tests in JwtAuthProcess class as it no longer extends JwtAuth class.

--- src/JwtAuthProcess.php
+++ src/JwtAuthProcess.php
@@ -4,25 +4,15 @@ declare(strict_types=1);
 
 namespace PsrJwt;
 
-use PsrJwt\JwtAuth;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class JwtAuthProcess extends JwtAuth implements MiddlewareInterface
+class JwtAuthProcess implements MiddlewareInterface
 {
-    public function __construct(string $tokenKey, string $secret)
-    {
-        parent::__construct($tokenKey, $secret);
-    }
-
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $token = $this->getToken($request);
-
-        $this->validate($token);
-
         return $handler->handle($request);
     }
 }
