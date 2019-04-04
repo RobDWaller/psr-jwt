1050c3d85a3107ef51c5e1eba12ba59b0410037e
RobDWaller <rdwaller1984@googlemail.com>
2019-04-04 14:49:54 +0100

    Moved business logic from defunct JwtAuth class to JwtAuthHandler, moved Auth tests from Invokable tests to Handler tests.

--- src/JwtAuthHandler.php
+++ src/JwtAuthHandler.php
@@ -8,11 +8,109 @@ use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Nyholm\Psr7\Factory\Psr17Factory;
+use Throwable;
 
 class JwtAuthHandler implements RequestHandlerInterface
 {
+    private $tokenKey;
+
+    private $secret;
+
+    public function __construct(string $tokenKey, string $secret)
+    {
+        $this->tokenKey = $tokenKey;
+
+        $this->secret = $secret;
+    }
+
+    protected function getSecret(): string
+    {
+        return $this->secret;
+    }
+
+    protected function validate(string $token): bool
+    {
+        $parse = Jwt::parser($token, $this->getSecret());
+
+        try {
+            $parse->validate()
+                ->validateExpiration();
+        } catch (Throwable $e) {
+            if (in_array($e->getCode(), [1, 2, 3, 4], true)) {
+                throw new JwtAuthException($e->getMessage(), $e->getCode());
+            }
+        }
+
+        try {
+            $parse->validateNotBefore();
+        } catch (Throwable $e) {
+            if (in_array($e->getCode(), [5], true)) {
+                throw new JwtAuthException($e->getMessage(), $e->getCode());
+            }
+        }
+
+        return true;
+    }
+
+    protected function hasJwt(array $data): bool
+    {
+        return array_key_exists($this->tokenKey, $data);
+    }
+
+    protected function getToken(ServerRequestInterface $request): string
+    {
+        $merge = array_merge(
+            $request->getServerParams(),
+            $request->getCookieParams(),
+            $request->getQueryParams(),
+            $this->parseRequestBody($request),
+            $this->parseBearerToken($request)
+        );
+
+        if ($this->hasJwt($merge)) {
+            return $merge[$this->tokenKey];
+        }
+
+        throw new JwtAuthException('JWT Token not set', 1);
+    }
+
+    private function parseRequestBody(ServerRequestInterface $request): array
+    {
+        if (is_array($request->getParsedBody()) && isset($request->getParsedBody()[$this->tokenKey])) {
+            return $request->getParsedBody();
+        }
+
+        if (is_object($request->getParsedBody()) && isset($request->getParsedBody()->jwt)) {
+            return [$this->tokenKey => $request->getParsedBody()->jwt];
+        }
+
+        return [];
+    }
+
+    private function parseBearerToken(ServerRequestInterface $request): array
+    {
+        $token = $this->getBearerToken($request);
+
+        return !empty($token) ? [$this->tokenKey => $token] : [];
+    }
+
+    private function getBearerToken(ServerRequestInterface $request): string
+    {
+        $authorization = $request->getHeader('authorization');
+
+        $bearer = array_filter($authorization, function($item) {
+            return (bool) preg_match('/^bearer\s.+/', $item);
+        });
+
+        return explode(' ', $authorization[0] ?? '')[1] ?? '';
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $token = $this->getToken($request);
+
+        $this->validate($token);
+
         return Psr17Factory::createResponse(200);
     }
 }
--- tests/JwtAuthHandlerTest.php
+++ tests/JwtAuthHandlerTest.php
@@ -4,16 +4,19 @@ namespace Test;
 
 use PHPUnit\Framework\TestCase;
 use PsrJwt\JwtAuthHandler;
+use PsrJwt\Jwt;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use ReflectionMethod;
 use Mockery as m;
+use stdClass;
 
 class JwtAuthHandlerTest extends TestCase
 {
     public function testJwtAuthHandler()
     {
-        $handler = new JwtAuthHandler();
+        $handler = new JwtAuthHandler('jwt', 'secret');
 
         $this->assertInstanceOf(JwtAuthHandler::class, $handler);
         $this->assertInstanceOf(RequestHandlerInterface::class, $handler);
@@ -21,12 +24,451 @@ class JwtAuthHandlerTest extends TestCase
 
     public function testJwtAuthHandlerResponse()
     {
-        $request = m::mock(ServerRequestInterface::class);
+        $jwt = Jwt::builder();
+        $token = $jwt->setSecret('Secret123!456$')
+            ->setIssuer('localhost')
+            ->build()
+            ->getToken();
 
-        $handler = new JwtAuthHandler();
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['jwt' => $token]);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getParsedBody')
+            ->twice()
+            ->andReturn([]);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'Secret123!456$');
 
         $result = $handler->handle($request);
 
         $this->assertInstanceOf(ResponseInterface::class, $result);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthHandler::hasJwt
+     */
+    public function testJwtAuthHandlerHasJwt()
+    {
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $data['jwt'] = 'abc.abc.abc';
+        $data['foo'] = 'bar';
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'hasJwt');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$data]);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthHandler::hasJwt
+     */
+    public function testJwtAuthHandlerHasJwtFalse()
+    {
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $data['token'] = 'abc.abc.abc';
+        $data['foo'] = 'bar';
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'hasJwt');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$data]);
+
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthHandler::hasJwt
+     */
+    public function testJwtAuthHandlerHasJwtEmpty()
+    {
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $data = [];
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'hasJwt');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$data]);
+
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthHandler::getToken
+     * @uses PsrJwt\JwtAuthHandler::hasJwt
+     */
+    public function testGetToken()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getParsedBody')
+            ->twice()
+            ->andReturn([]);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$request]);
+
+        $this->assertSame($result, 'abc.def.ghi');
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::getToken
+     * @uses PsrJwt\JwtAuth::hasJwt
+     */
+    public function testGetTokenFromCookie()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getParsedBody')
+            ->twice()
+            ->andReturn([]);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$request]);
+
+        $this->assertSame($result, 'abc.def.ghi');
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::getToken
+     * @uses PsrJwt\JwtAuth::hasJwt
+     */
+    public function testGetTokenFromQuery()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+        $request->shouldReceive('getParsedBody')
+            ->twice()
+            ->andReturn([]);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$request]);
+
+        $this->assertSame($result, 'abc.def.ghi');
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::getToken
+     * @uses PsrJwt\JwtAuth::hasJwt
+     */
+    public function testGetTokenFromBody()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['car' => 'park']);
+        $request->shouldReceive('getParsedBody')
+            ->times(3)
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$request]);
+
+        $this->assertSame($result, 'abc.def.ghi');
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::getToken
+     * @uses PsrJwt\JwtAuth::hasJwt
+     */
+    public function testGetTokenFromBodyObject()
+    {
+        $object = new stdClass();
+        $object->jwt = 'abc.def.ghi';
+
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['car' => 'park']);
+        $request->shouldReceive('getParsedBody')
+            ->times(3)
+            ->andReturn($object);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$request]);
+
+        $this->assertSame($result, 'abc.def.ghi');
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::getToken
+     * @uses PsrJwt\JwtAuth::hasJwt
+     * @expectedException PsrJwt\JwtAuthException
+     * @expectedExceptionMessage JWT Token not set
+     */
+    public function testGetTokenFromBodyNull()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['car' => 'park']);
+        $request->shouldReceive('getParsedBody')
+            ->once()
+            ->andReturn(null);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $method->invokeArgs($handler, [$request]);
+    }
+
+    public function testGetTokenFromBearer()
+    {
+        $object = new stdClass();
+        $object->jwt = 'abc.def.ghi';
+
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['car' => 'park']);
+        $request->shouldReceive('getParsedBody')
+            ->times(3)
+            ->andReturn(null);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn(['bearer abc.def.ghi']);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$request]);
+
+        $this->assertSame($result, 'abc.def.ghi');
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::getToken
+     * @uses PsrJwt\JwtAuth::hasJwt
+     * @uses PsrJwt\JwtAuthException::__construct
+     * @expectedException PsrJwt\JwtAuthException
+     * @expectedMessage JWT Token not set
+     * @expectedExceptionCode 1
+     */
+    public function testGetTokenNoJwt()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['hello' => 'world']);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['car' => 'park']);
+        $request->shouldReceive('getParsedBody')
+            ->twice()
+            ->andReturn(['gary' => 'barlow']);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getToken');
+        $method->setAccessible(true);
+        $method->invokeArgs($handler, [$request]);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthHandler::getSecret
+     */
+    public function testGetSecret()
+    {
+        $handler = new JwtAuthHandler('jwt', 'secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'getSecret');
+        $method->setAccessible(true);
+        $result = $method->invoke($handler);
+
+        $this->assertSame('secret', $result);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthHandler::validate
+     */
+    public function testValidate()
+    {
+        $jwt = Jwt::builder();
+        $token = $jwt->setSecret('Secret123!456$')
+            ->setIssuer('localhost')
+            ->build()
+            ->getToken();
+
+        $handler = new JwtAuthHandler('jwt', 'Secret123!456$');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'validate');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($handler, [$token]);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::validate
+     * @expectedException PsrJwt\JwtAuthException
+     * @expectedExceptionMessage Signature is invalid.
+     * @expectedExceptionCode 3
+     */
+    public function testValidateBadSecret()
+    {
+        $jwt = Jwt::builder();
+        $token = $jwt->setSecret('Secret123!456$')
+            ->setIssuer('localhost')
+            ->build()
+            ->getToken();
+
+        $handler = new JwtAuthHandler('jwt', 'Secret');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'validate');
+        $method->setAccessible(true);
+        $method->invokeArgs($handler, [$token]);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::validate
+     * @expectedException PsrJwt\JwtAuthException
+     * @expectedExceptionMessage Expiration claim has expired.
+     * @expectedExceptionCode 4
+     */
+    public function testValidateBadExpiration()
+    {
+        $jwt = Jwt::builder();
+        $token = $jwt->setSecret('Secret123!456$')
+            ->setIssuer('localhost')
+            ->setPayloadClaim('exp', time() - 10)
+            ->build()
+            ->getToken();
+
+        $handler = new JwtAuthHandler('jwt', 'Secret123!456$');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'validate');
+        $method->setAccessible(true);
+        $method->invokeArgs($handler, [$token]);
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuth::validate
+     * @expectedException PsrJwt\JwtAuthException
+     * @expectedExceptionMessage Not Before claim has not elapsed.
+     * @expectedExceptionCode 5
+     */
+    public function testValidateBadNotBefore()
+    {
+        $jwt = Jwt::builder();
+        $token = $jwt->setSecret('Secret123!456$')
+            ->setIssuer('localhost')
+            ->setPayloadClaim('nbf', time() + 60)
+            ->build()
+            ->getToken();
+
+        $handler = new JwtAuthHandler('jwt', 'Secret123!456$');
+
+        $method = new ReflectionMethod(JwtAuthHandler::class, 'validate');
+        $method->setAccessible(true);
+        $method->invokeArgs($handler, [$token]);
     }
 }
