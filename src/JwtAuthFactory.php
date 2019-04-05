5eb5494f8d20a4553fb02bf1f615ea1ec46bfabd
RobDWaller <rdwaller1984@googlemail.com>
2019-04-05 16:29:18 +0100

    Added invokable and handler methods for relevant object generation in JwtAuthFactory class, and wrote relevant tests.

--- src/JwtAuthFactory.php
+++ src/JwtAuthFactory.php
@@ -5,11 +5,25 @@ declare(strict_types=1);
 namespace PsrJwt;
 
 use PsrJwt\JwtAuthMiddleware;
+use PsrJwt\JwtAuthHandler;
+use PsrJwt\JwtAuthInvokable;
 
 class JwtAuthFactory
 {
     public static function middleware(): JwtAuthMiddleware
     {
         return new JwtAuthMiddleware();
+    }
+
+    public function handler($tokenKey, $secret): JwtAuthHandler
+    {
+        return new JwtAuthHandler($tokenKey, $secret);
+    }
+
+    public function invokable($tokenKey, $secret): JwtAuthInvokable
+    {
+        $handler = new JwtAuthHandler($tokenKey, $secret);
+
+        return new JwtAuthInvokable($handler);
     }
 }
--- tests/JwtAuthFactoryTest.php
+++ tests/JwtAuthFactoryTest.php
@@ -5,17 +5,41 @@ namespace Test;
 use PHPUnit\Framework\TestCase;
 use PsrJwt\JwtAuthFactory;
 use PsrJwt\JwtAuthMiddleware;
+use PsrJwt\JwtAuthHandler;
+use PsrJwt\JwtAuthInvokable;
 
 class JwtAuthFactoryTest extends TestCase
 {
     /**
-     * @covers PsrJwt\JwtAuthFactory::Middleware
+     * @covers PsrJwt\JwtAuthFactory::middleware
      */
     public function testJwtAuthFactoryMiddleware()
     {
         $this->assertInstanceOf(
             JwtAuthMiddleware::class,
             JwtAuthFactory::middleware()
+        );
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthFactory::invokable
+     */
+    public function testJwtAuthFactoryInvokable()
+    {
+        $this->assertInstanceOf(
+            JwtAuthInvokable::class,
+            JwtAuthFactory::invokable('jwt', '$Secret123!')
+        );
+    }
+
+    /**
+     * @covers PsrJwt\JwtAuthFactory::handler
+     */
+    public function testJwtAuthFactoryHandler()
+    {
+        $this->assertInstanceOf(
+            JwtAuthHandler::class,
+            JwtAuthFactory::handler('jwt', '$Secret123!')
         );
     }
 }
