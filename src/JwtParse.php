4aa631a29e0dfdf3fbfaeddb4f6e92d5c6e34276
RobDWaller <rdwaller1984@googlemail.com>
2019-04-15 07:45:48 +0100

    Finished initial tests and functionality for JwtParse class.

--- src/JwtParse.php
+++ src/JwtParse.php
@@ -4,7 +4,85 @@ declare(strict_types = 1);
 
 namespace PsrJwt;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class JwtParse
 {
-    
+    private const LOCATIONS = [
+        'Bearer',
+        'Cookie',
+        'Query',
+        'Body',
+        'Server'
+    ];
+
+    private $tokenKey;
+
+    public function __construct(string $tokenKey)
+    {
+        $this->tokenKey = $tokenKey;
+    }
+
+    public function findToken(ServerRequestInterface $request): array
+    {
+        foreach (self::LOCATIONS as $location) {
+            $jwtArray = $this->{'getFrom' . $location}($request);
+
+            if (isset($jwtArray[$this->tokenKey])) {
+                return $jwtArray;
+            }
+        }
+
+        return [];
+    }
+
+    private function getFromBearer(ServerRequestInterface $request): array
+    {
+        $authorization = $request->getHeader('authorization');
+
+        $bearer = array_filter($authorization, function ($item) {
+            return (bool) preg_match('/^bearer\s.+/', $item);
+        });
+
+        $token = explode(' ', $bearer[0] ?? '')[1] ?? '';
+
+        return !empty($token) ? ['jwt' => $token] : [];
+    }
+
+    private function getFromCookie(ServerRequestInterface $request): array
+    {
+        return $request->getCookieParams();
+    }
+
+    private function getFromQuery(ServerRequestInterface $request): array
+    {
+        return $request->getQueryParams();
+    }
+
+    private function getFromBody(ServerRequestInterface $request): array
+    {
+        $body = $request->getParsedBody();
+
+        if (is_array($body) && isset($body[$this->tokenKey])) {
+            return $body;
+        }
+
+        return $this->parseBodyObject($request);
+    }
+
+    private function parseBodyObject(ServerRequestInterface $request): array
+    {
+        $body = $request->getParsedBody();
+
+        if (is_object($body) && isset($body->{$this->tokenKey})) {
+            return [$this->tokenKey => $body->{$this->tokenKey}];
+        }
+
+        return [];
+    }
+
+    private function getFromServer(ServerRequestInterface $request): array
+    {
+        return $request->getServerParams();
+    }
 }
--- tests/JwtParseTest.php
+++ tests/JwtParseTest.php
@@ -4,7 +4,6 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 use PsrJwt\JwtParse;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use ReflectionMethod;
 use Mockery as m;
@@ -14,7 +13,111 @@ class JwtParseTest extends TestCase
 {
     public function testJwtParse()
     {
-        $parse = new JwtParse();
+        $parse = new JwtParse('jwt');
         $this->assertInstanceOf(JwtParse::class, $parse);
+    }
+
+    public function testFindToken()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn(['bearer abc.def.ghi']);
+
+        $parse = new JwtParse('jwt');
+        $result = $parse->findToken($request);
+
+        $this->assertSame(['jwt' => 'abc.def.ghi'], $result);
+    }
+
+    public function testFindTokenCookie()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+
+        $parse = new JwtParse('jwt');
+        $result = $parse->findToken($request);
+
+        $this->assertSame(['jwt' => 'abc.def.ghi'], $result);
+    }
+
+    public function testFindTokenQuery()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+
+        $parse = new JwtParse('jwt');
+        $result = $parse->findToken($request);
+
+        $this->assertSame(['jwt' => 'abc.def.ghi'], $result);
+    }
+
+    public function testFindTokenBody()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getParsedBody')
+            ->once()
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+
+        $parse = new JwtParse('jwt');
+        $result = $parse->findToken($request);
+
+        $this->assertSame(['jwt' => 'abc.def.ghi'], $result);
+    }
+
+    public function testFindTokenServer()
+    {
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getParsedBody')
+            ->twice()
+            ->andReturn([]);
+        $request->shouldReceive('getServerParams')
+            ->once()
+            ->andReturn(['jwt' => 'abc.def.ghi']);
+
+        $parse = new JwtParse('jwt');
+        $result = $parse->findToken($request);
+
+        $this->assertSame(['jwt' => 'abc.def.ghi'], $result);
+    }
+
+    public function tearDown() {
+        m::close();
     }
 }
