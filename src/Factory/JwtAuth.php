6b34848c15200fb46bde79ed29bf06c409bc7ded
RobDWaller <rdwaller1984@googlemail.com>
2019-05-08 22:04:39 +0100

    Added comments to the Factory namespace.

--- src/Factory/JwtAuth.php
+++ src/Factory/JwtAuth.php
@@ -8,8 +8,20 @@ use PsrJwt\JwtAuthMiddleware;
 use PsrJwt\Auth\Authenticate;
 use PsrJwt\JwtAuthInvokable;
 
+/**
+ * Easily add the PSR-JWT middleware to PSR compliant frameworks such as
+ * Zend Expressive and Slim PHP.
+ */
 class JwtAuth
 {
+    /**
+     * Add the middleware to the relevant framework.
+     *
+     * @param string $tokenKey
+     * @param string $secret
+     * @return JwtAuthMiddleware
+     * @todo TokenKey and Secret are the wrong way around.
+     */
     public static function middleware(string $tokenKey, string $secret): JwtAuthMiddleware
     {
         $auth = new Authenticate($tokenKey, $secret);
