bf7be807a9780c9b28cd0b964dd2b87d91efdfcc
RobDWaller <rdwaller1984@googlemail.com>
2019-05-17 08:02:20 +0100

    Added comments to Auth Handler class.

--- src/Handler/Auth.php
+++ src/Handler/Auth.php
@@ -10,10 +10,24 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Nyholm\Psr7\Response;
 
+/**
+ * Default JWT authentication handler. Allows you to customise body response
+ * with a simple message. If you require a more detailed response create your
+ * own handler which extends the Authenticate class and calls the authenticate
+ * method as below. 
+ */
 class Auth extends Authenticate implements RequestHandlerInterface
 {
+    /**
+     * @var string The content to add to the response body.
+     */
     private $body;
 
+    /**
+     * @param string $secret
+     * @param string $tokenKey
+     * @param string $body
+     */
     public function __construct(string $secret, string $tokenKey, string $body)
     {
         parent::__construct($secret, $tokenKey);
@@ -21,6 +35,10 @@ class Auth extends Authenticate implements RequestHandlerInterface
         $this->body = $body;
     }
 
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $auth = $this->authenticate($request);
