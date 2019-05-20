a0ccde68bf6aa0b38e30864cb8354a6873d3a591
RobDWaller <rdwaller1984@googlemail.com>
2019-05-20 11:47:22 +0100

    Added test to check what happens when an empty body is passed to the auth handler.

--- tests/Handler/AuthTest.php
+++ tests/Handler/AuthTest.php
@@ -85,6 +85,52 @@ class AuthTest extends TestCase
      * @uses PsrJwt\Parser\Query
      * @uses PsrJwt\Parser\Parse
      */
+    public function testAuthenticateNoBody()
+    {
+        $jwt = Jwt::builder();
+        $token = $jwt->setSecret('Secret123!456$')
+            ->setIssuer('localhost')
+            ->build()
+            ->getToken();
+
+        $request = m::mock(ServerRequestInterface::class);
+        $request->shouldReceive('getHeader')
+            ->with('authorization')
+            ->once()
+            ->andReturn([]);
+        $request->shouldReceive('getCookieParams')
+            ->once()
+            ->andReturn(['foo' => 'bar']);
+        $request->shouldReceive('getParsedBody')
+            ->twice()
+            ->andReturn([]);
+        $request->shouldReceive('getQueryParams')
+            ->once()
+            ->andReturn(['jwt' => $token]);
+
+        $auth = new Auth('Secret123!456$', 'jwt', '');
+
+        $result = $auth->handle($request);
+
+        $this->assertInstanceOf(ResponseInterface::class, $result);
+        $this->assertSame(200, $result->getStatusCode());
+        $this->assertSame('Ok', $result->getReasonPhrase());
+        $this->assertEmpty($result->getBody()->__toString());
+    }
+
+    /**
+     * @covers PsrJwt\Handler\Auth::handle
+     * @uses PsrJwt\Handler\Auth::__construct
+     * @uses PsrJwt\Factory\Jwt
+     * @uses PsrJwt\Auth\Authenticate
+     * @uses PsrJwt\Auth\Auth
+     * @uses PsrJwt\Validation\Validate
+     * @uses PsrJwt\Parser\Bearer
+     * @uses PsrJwt\Parser\Cookie
+     * @uses PsrJwt\Parser\Body
+     * @uses PsrJwt\Parser\Query
+     * @uses PsrJwt\Parser\Parse
+     */
     public function testAuthenticateBadRequest()
     {
         $jwt = Jwt::builder();
