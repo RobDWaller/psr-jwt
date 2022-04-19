2a46e032eecff61f0c76f8b382791348bc6b036a
Rob Waller <rdwaller1984@gmail.com>
2022-04-19 17:03:29 +0100

    Added expiration and signature checks to the validate class.

--- src/Validation/Validate.php
+++ src/Validation/Validate.php
@@ -32,7 +32,8 @@ class Validate
     {
         try {
             $this->validate->structure()
-                ->expiration();
+                ->expiration()
+                ->signature();
         } catch (ValidateException $e) {
             if (in_array($e->getCode(), [1, 2, 3, 4], true)) {
                 return ['code' => $e->getCode(), 'message' => $e->getMessage()];
