79506d42c3b1de4e20efaa4635b72027e9909a21
Rob Waller <rdwaller1984@gmail.com>
2022-06-11 22:14:16 +0100

    Added and configured PHP CS Fixer to CI pipeline.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()->in(
+    [__DIR__ . '/src', __DIR__ . '/tests']
+);
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+        '@PSR12' => true,
+        'no_unused_imports' => true,
+        'declare_strict_types' => true,
+        'php_unit_strict' => true,
+    ])->setFinder($finder);
