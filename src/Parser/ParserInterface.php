5341d8c2a347aa44434227d4a8be48eab1cd2c3f
Rob Waller <rdwaller1984@gmail.com>
2022-06-13 12:55:02 +0100

    Fixed PSR-12 issue in ParserInterface.

--- src/Parser/ParserInterface.php
+++ src/Parser/ParserInterface.php
@@ -7,7 +7,7 @@ namespace PsrJwt\Parser;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * The parse method is used to retrieve the JWT token from the request object. 
+ * The parse method is used to retrieve the JWT token from the request object.
  */
 interface ParserInterface
 {
