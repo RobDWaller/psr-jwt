c993bcf1c89a5b8f51c8aff57c2526314747125e
Rob Waller <rdwaller1984@gmail.com>
2022-06-16 22:57:39 +0100

    Added additional tests for Authorise class.

--- tests/Auth/AuthoriseTest.php
+++ tests/Auth/AuthoriseTest.php
@@ -51,4 +51,57 @@ class AuthoriseTest extends TestCase
         $this->assertSame(400, $result->getCode());
         $this->assertSame('Bad Request: Expiration claim has expired.', $result->getMessage());
     }
+
+    /**
+     * @covers PsrJwt\Auth\Authorise::authorise
+     */
+    public function testAuthoriseBadSignature(): void
+    {
+        $jwt = new Jwt();
+
+        $authorise = new Authorise();
+        $result = $authorise->authorise('123.abc.456', 'Secret123!456$');
+
+        $this->assertSame(400, $result->getCode());
+        $this->assertSame('Bad Request: Signature is invalid.', $result->getMessage());
+    }
+
+    /**
+     * @covers PsrJwt\Auth\Authorise::authorise
+     */
+    public function testValidateNotBefore(): void
+    {
+        $jwt = new Jwt();
+        $builder = $jwt->builder('Secret123!456$');
+        $token = $builder->setIssuer('localhost')
+            ->setExpiration(time() + 20)
+            ->setPayloadClaim('nbf', time() + 10)
+            ->build()
+            ->getToken();
+
+        $authorise = new Authorise();
+        $result = $authorise->authorise($token, 'Secret123!456$');
+
+        $this->assertSame(400, $result->getCode());
+        $this->assertSame('Bad Request: Not Before claim has not elapsed.', $result->getMessage());
+    }
+
+    /**
+     * @covers PsrJwt\Auth\Authorise::authorise
+     */
+    public function testValidateNotBeforeOk(): void
+    {
+        $jwt = new Jwt();
+        $builder = $jwt->builder('Secret123!456$');
+        $token = $builder->setIssuer('localhost')
+            ->setPayloadClaim('nbf', time() - 20)
+            ->build()
+            ->getToken();
+
+        $authorise = new Authorise();
+        $result = $authorise->authorise($token, 'Secret123!456$');
+
+        $this->assertSame(200, $result->getCode());
+        $this->assertSame('Ok', $result->getMessage());
+    }
 }
