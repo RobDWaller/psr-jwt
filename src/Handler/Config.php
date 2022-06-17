72fe6848d92be34be6054b9e944b1d976a66e28a
Rob Waller <rdwaller1984@gmail.com>
2022-06-17 13:19:26 +0100

    Created Config Handler class to configure the handlers.

--- src/Handler/Config.php
+++ src/Handler/Config.php
@@ -8,13 +8,31 @@ class Config
 {
     private string $secret;
 
-    public function __construct(string $secret)
+    private string $key;
+
+    private string | array $response;
+
+    public function __construct(string $secret, string $key, string | array $response)
     {
         $this->secret = $secret;
+
+        $this->key = $key;
+
+        $this->response = $response;
     }
 
     public function getSecret(): string
     {
         return $this->secret;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function getResponse(): string | array
+    {
+        return $this->response;
     }
 }
--- tests/Handler/ConfigTest.php
+++ tests/Handler/ConfigTest.php
@@ -14,8 +14,40 @@ class ConfigTest extends TestCase
      */
     public function testGetSecret(): void
     {
-        $config = new Config('123');
+        $config = new Config('123', 'Bearer', 'Bar');
 
         $this->assertSame('123', $config->getSecret());
+    }
+
+    /**
+     * @covers PsrJwt\Handler\Config::getKey
+     */
+    public function testGetKey(): void
+    {
+        $config = new Config('456', 'JWT', 'Foo');
+
+        $this->assertSame('JWT', $config->getKey());
+    }
+
+    /**
+     * @covers PsrJwt\Handler\Config::getResponse
+     */
+    public function testGetResponseAsString(): void
+    {
+        $config = new Config('456', 'JWT', 'Hello');
+
+        $this->assertSame('Hello', $config->getResponse());
+    }
+
+    /**
+     * @covers PsrJwt\Handler\Config::getResponse
+     */
+    public function testGetResponseAsArray(): void
+    {
+        $config = new Config('456', 'JWT', ['Hello', 'World']);
+
+        $this->assertCount(2, $config->getResponse());
+        $this->assertSame('Hello', $config->getResponse()[0]);
+        $this->assertSame('World', $config->getResponse()[1]);
     }
 }
